ee0ddc45c833052a53233960a0a2a4342dff6bd6
RafaRed <rafaelszcardoso@gmail.com>
2022-02-05 19:01:38 -0300

    Quiz Screen

--- web3/src/index.js
+++ web3/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Welcome from './Welcome';
+import Quiz from './Quiz';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Web3ReactProvider } from "@web3-react/core"
 import { Web3Provider } from "@ethersproject/providers";
@@ -18,6 +19,7 @@ ReactDOM.render(
     <Routes>
       <Route exact path="/" element={<App/>} />
       <Route exact path="/welcome" element={<Welcome/>} />
+      <Route exact path="/quiz" element={<Quiz/>} />
       </Routes>
     </Router>
     </Web3ReactProvider>,
