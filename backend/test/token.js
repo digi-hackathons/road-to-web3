13576d7ba1510ae046cbf2202530e56220935c37
digiront <digiront@protonmail.com>
2022-02-05 16:57:11 -0500

    Deployment with hardhat.

---
+++ backend/test/token.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Placeholder", function () {
+  it("Should test something", async function () {
+  });
+});
---
+++ backend/utils/constants.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    TOKEN_NAME: 'LingoSwap',
+    TOKEN_SYMBOL: 'LINGO',
+    TOKEN_CONTRACT: 'LingSwapToken',
+});
