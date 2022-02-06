e94124e8c00e0dfe8fdbfc9029b78327c456406d
RafaRed <rafaelszcardoso@gmail.com>
2022-02-06 19:47:04 -0300

    Working in new navbar

--- web3/src/components/Navbar.js
+++ web3/src/components/Navbar.js
@@ -6,10 +6,13 @@ import {useWeb3React} from "@web3-react/core";
 
 
 
+<<<<<<< HEAD
 function redirect(){
     window.location.href = "/quiz";
 }
 
+=======
+>>>>>>> d4bc5bc (Working in new navbar)
 
 function Profile(props){
 
