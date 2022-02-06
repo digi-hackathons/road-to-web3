6e320d4cbcf591a022000faabc21bbf6006159a8
digiront <digiront@protonmail.com>
2022-02-06 02:59:23 -0500

    Make storage contract ownable

--- backend/contracts/LingoStorage.sol
+++ backend/contracts/LingoStorage.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.8.0;
 
-contract LingoStorage {
-    // https://gateway.pinata.cloud/ipfs/QmRqcTyPLu2PCfAwQEZN121myWEYpb5E5udKLygBYYUVVL?preview=1
-    string constant images_dir_cid = "QmRqcTyPLu2PCfAwQEZN121myWEYpb5E5udKLygBYYUVVL";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LingoStorage is Ownable {
+
+    string images_dir_cid = "QmRqcTyPLu2PCfAwQEZN121myWEYpb5E5udKLygBYYUVVL";
+
+    function set_metadata_cid(string calldata cid) public onlyOwner () {
+        images_dir_cid = cid;
+    }
+
     function get_images_dir_cid() public view returns (string memory) {
         return images_dir_cid;
     }
