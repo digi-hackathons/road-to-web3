54bb67c51204ebb4bbe01958ad295e5318efe655
RafaRed <rafaelszcardoso@gmail.com>
2022-02-06 14:56:35 -0300

    Rewards page complete

---
+++ web3/src/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import "./css/Home.css";
+
+function Home() {
+  return <div></div>;
+}
+
+export default Home;
